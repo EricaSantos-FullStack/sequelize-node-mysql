03373a3481c2dde6e3446f15d5a024e0d9fad102
Erica S. Francisco <erykatdb@yahoo.com.br>
2023-06-17 00:27:22 -0300

    feat: 🎸 models and db relations

---
+++ api/models/niveis.js
@@ -0,0 +1,16 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Niveis = sequelize.define(
+    "Niveis",
+    {
+      descr_nivel: DataTypes.STRING,
+    },
+    {}
+  );
+  Niveis.associate = function (models) {
+    Niveis.hasMany(models.Turmas, {
+      foreignKey: "nivel_id",
+    });
+  };
+  return Niveis;
+};
