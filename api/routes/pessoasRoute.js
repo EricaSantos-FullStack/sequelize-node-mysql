70bfe4aec462628252a4db72c3d008da4a9e9b04
Erica S. Francisco <erykatdb@yahoo.com.br>
2023-06-25 14:39:39 -0300

    feat: 🎸 routes

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -10,5 +10,19 @@ router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
 router.post("/pessoas", PessoaController.criaPessoa);
 router.put("/pessoas/:id", PessoaController.atualizaPessoa);
 router.delete("/pessoas/:id", PessoaController.apagaPessoa);
+// as matriculos estão sempre vinculadas a uma pessoa, assim sendo os métodos de matricula são feitos no controller Pessoas e as rotas em pessoasRoute
+router.get(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.pegaUmaMatricula
+);
+router.post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula);
+router.put(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.atualizaMatricula
+);
+router.delete(
+  "/pessoas/:estudanteId/matricula/:matriculaId",
+  PessoaController.apagaMatricula
+);
 
 module.exports = router;
