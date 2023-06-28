385dc4b27fb262280ebadc7bebd00f4af028bd1e
Erica S. Francisco <erykatdb@yahoo.com.br>
2023-06-28 15:27:50 -0300

    feat: 🎸 recover, show active, soft delete and hard delete

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -2,9 +2,18 @@
 const database = require("../models");
 
 class PessoaController {
+  static async pegaPessoasAtivas(req, res) {
+    try {
+      const pessoasAtivas = await database.Pessoas.findAll();
+      return res.status(200).json(pessoasAtivas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
   static async pegaTodasAsPessoas(req, res) {
     try {
-      const todasAsPessoas = await database.Pessoas.findAll();
+      const todasAsPessoas = await database.Pessoas.scope("todos").findAll();
       return res.status(200).json(todasAsPessoas);
     } catch (error) {
       return res.status(500).json(error.message);
@@ -62,6 +71,31 @@ class PessoaController {
     }
   }
 
+  static async hardDeletePessoa(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.destroy({
+        where: { id: Number(id) },
+        force: true,
+      });
+      return res
+        .status(200)
+        .json({ mensagem: `id ${id} deletado definitivamente` });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async restauraPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.restore({ where: { id: Number(id) } });
+      return res.status(200).json({ mensagem: `id ${id} restaurado` });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
   // as matriculos estão sempre vinculadas a uma pessoa, assim sendo os métodos de matricula são feitos no controller Pessoas
   static async pegaUmaMatricula(req, res) {
     const { estudanteId, matriculaId } = req.params;
@@ -73,6 +107,21 @@ class PessoaController {
         },
       });
       return res.status(200).json(umaMatricula);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async restauraMatricula(req, res) {
+    const { estudanteId, matriculaId } = req.params;
+    try {
+      await database.Matriculas.restore({
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId),
+        },
+      });
+      return res.status(200).json({ mensagem: `id ${id} restaurado` });
     } catch (error) {
       return res.status(500).json(error.message);
     }
