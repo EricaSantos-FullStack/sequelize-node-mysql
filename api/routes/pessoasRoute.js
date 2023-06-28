fdf09bfefcf05b4dbc52f50ae57f7fc9ca0462a9
Erica S. Francisco <erykatdb@yahoo.com.br>
2023-06-28 15:29:24 -0300

    feat: 🎸 update new routes

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,17 +5,25 @@ const PessoaController = require("../controllers/PessoaController");
 const router = Router();
 
 //ao invés de chamar um callback, chamaremos a classe
-router.get("/pessoas", PessoaController.pegaTodasAsPessoas);
+router.get("/pessoas", PessoaController.pegaPessoasAtivas);
+router.get("/pessoas/todos", PessoaController.pegaTodasAsPessoas);
 router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
 router.post("/pessoas", PessoaController.criaPessoa);
+router.post("/pessoas/:id/restaura", PessoaController.restauraPessoa);
 router.put("/pessoas/:id", PessoaController.atualizaPessoa);
 router.delete("/pessoas/:id", PessoaController.apagaPessoa);
+router.delete("/pessoas/:id/hardDelete", PessoaController.hardDeletePessoa);
+
 // as matriculos estão sempre vinculadas a uma pessoa, assim sendo os métodos de matricula são feitos no controller Pessoas e as rotas em pessoasRoute
 router.get(
   "/pessoas/:estudanteId/matricula/:matriculaId",
   PessoaController.pegaUmaMatricula
 );
 router.post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula);
+router.post(
+  "/pessoas/:estudanteId/matricula/:matriculaId/restaura",
+  PessoaController.restauraMatricula
+);
 router.put(
   "/pessoas/:estudanteId/matricula/:matriculaId",
   PessoaController.atualizaMatricula
