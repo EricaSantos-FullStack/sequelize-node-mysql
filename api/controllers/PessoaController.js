1fa92637649299e5209c6f8d91d2d9b12bab3b1d
Erica S. Francisco <erykatdb@yahoo.com.br>
2023-07-05 13:05:38 -0300

    feat: 🎸 new functions

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,5 +1,6 @@
 //por padrão o javascript procurará o arquivo index na pasta modelo
 const database = require("../models");
+const Sequelize = require("sequelize");
 
 class PessoaController {
   static async pegaPessoasAtivas(req, res) {
@@ -161,6 +162,73 @@ class PessoaController {
     try {
       await database.Matriculas.destroy({ where: { id: Number(matriculaId) } });
       return res.status(200).json({ mensagem: `id ${matriculaId} deletado` });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async pegaMatriculas(req, res) {
+    const { estudanteId } = req.params;
+    try {
+      const pessoa = await database.Pessoas.findOne({
+        where: { id: Number(estudanteId) },
+      });
+      const matriculas = await pessoa.getAulasMatriculadas();
+      return res.status(200).json(matriculas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async pegaMatriculasPorTurma(req, res) {
+    const { turmaId } = req.params;
+    try {
+      const todasAsMatriculas = await database.Matriculas.findAndCountAll({
+        where: {
+          turma_id: Number(turmaId),
+          status: "confirmado",
+        },
+        limit: 20,
+        order: [["estudante_id", "ASC"]], //a ordem funciona dentro de 2 arrays -> ASC ordem ascendente
+      });
+      return res.status(200).json(todasAsMatriculas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async pegaTurmasLotadas(req, res) {
+    const lotacaoTurma = 2;
+    try {
+      const turmasLotadas = await database.Matriculas.findAndCountAll({
+        where: {
+          status: "confirmado",
+        },
+        attributes: ["turma_id"],
+        group: ["turma_id"],
+        having: Sequelize.literal(`count(turma_id) >= ${lotacaoTurma}`),
+      });
+      return res.status(200).json(turmasLotadas.count);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async cancelaPessoa(req, res) {
+    const { estudanteId } = req.params;
+    try {
+      await database.Pessoas.update(
+        { ativo: false },
+        { where: { id: Number(estudanteId) } }
+      );
+      await database.Matriculas.update(
+        { status: "cancelado" },
+        { where: { estudante_id: Number(estudanteId) } }
+      );
+      //updates costumam retornar apenas 0 e 1. Assim sendo peço para o json returnar uma mensagem de confirmação caso a requisição seja confirmada
+      return res.status(200).json({
+        message: `Matrículas ref. estudante ${estudanteId} canceladas`,
+      });
     } catch (error) {
       return res.status(500).json(error.message);
     }
