94413db5a402740a76b800273cb8eb2a7c8b15cf
Erica S. Francisco <erykatdb@yahoo.com.br>
2023-07-05 13:06:18 -0300

    feat: 🎸 routes

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -14,16 +14,27 @@ router.put("/pessoas/:id", PessoaController.atualizaPessoa);
 router.delete("/pessoas/:id", PessoaController.apagaPessoa);
 router.delete("/pessoas/:id/hardDelete", PessoaController.hardDeletePessoa);
 
-// as matriculos estão sempre vinculadas a uma pessoa, assim sendo os métodos de matricula são feitos no controller Pessoas e as rotas em pessoasRoute
+// as matriculas estão sempre vinculadas a uma pessoa, assim sendo os métodos de matricula são feitos no controller Pessoas e as rotas em pessoasRoute
 router.get(
   "/pessoas/:estudanteId/matricula/:matriculaId",
   PessoaController.pegaUmaMatricula
 );
+router.get("/pessoas/:estudanteId/matriculas", PessoaController.pegaMatriculas);
+router.get(
+  "/pessoas/matricula/:turmaId/confirmadas",
+  PessoaController.pegaMatriculasPorTurma
+);
+
+router.get("/pessoas/matricula/lotada", PessoaController.pegaTurmasLotadas);
+
 router.post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula);
 router.post(
   "/pessoas/:estudanteId/matricula/:matriculaId/restaura",
   PessoaController.restauraMatricula
 );
+
+router.post("/pessoas/:estudanteId/cancela", PessoaController.cancelaPessoa);
+
 router.put(
   "/pessoas/:estudanteId/matricula/:matriculaId",
   PessoaController.atualizaMatricula
