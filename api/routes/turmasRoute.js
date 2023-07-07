6e157042551844b261a7f007d625c67dc26dce7c
Erica S. Francisco <erykatdb@yahoo.com.br>
2023-07-07 12:35:09 -0300

    refactor: 💡 better writting in the path

--- api/routes/turmasRoute.js
+++ api/routes/turmasRoute.js
@@ -4,9 +4,10 @@ const TurmaController = require("../controllers/TurmaController");
 const router = Router();
 router
   .get("/turmas", TurmaController.pegaTodasAsTurmas)
-  .get("/turmas/:id", TurmaController.pegaUmaTurma)
+  .get("/turmas/:id", TurmaController.pegaTurma)
   .post("/turmas", TurmaController.criaTurma)
   .post("/turmas/:id/restaura", TurmaController.restauraTurma)
   .put("/turmas/:id", TurmaController.atualizaTurma)
   .delete("/turmas/:id", TurmaController.apagaTurma);
+
 module.exports = router;
