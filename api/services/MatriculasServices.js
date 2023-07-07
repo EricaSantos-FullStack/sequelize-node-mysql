4c865683b763b8c09a723849d2ec9551d844a518
Erica S. Francisco <erykatdb@yahoo.com.br>
2023-07-07 12:31:07 -0300

    feat: 🎸 new services Matricula, Niveis and turmas

---
+++ api/services/MatriculasServices.js
@@ -0,0 +1,10 @@
+const Services = require("./Services");
+
+class MatriculasServices extends Services {
+  constructor() {
+    super("Matriculas");
+  }
+  // método especifico do controlador de Matriculas
+}
+
+module.exports = MatriculasServices;
---
+++ api/services/TurmasServices.js
@@ -0,0 +1,10 @@
+const Services = require("./Services");
+
+class TurmasServices extends Services {
+  constructor() {
+    super("Turmas");
+  }
+  // método especifico do controlador de Turmas
+}
+
+module.exports = TurmasServices;
